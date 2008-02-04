041d95b54744eb89faa8577aae96e265e664caea
Sergio Ortiz Rojas <sergio.ortiz@gmail.com>
2008-02-04 15:48:26 +0000

    changes in docx
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@3604 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/apertium-header.sh
+++ apertium/apertium-header.sh
@@ -51,7 +51,7 @@ function translate_odt
   OTRASALIDA=/tmp/$$odtsalida.zip
   
   unzip -q -o -d $INPUT_TMPDIR $FICHERO
-  find $INPUT_TMPDIR | grep content\\.xml |\
+  find $INPUT_TMPDIR | grep content\\\.xml |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   $APERTIUM_PATH/apertium-desodt |\
   if [ ! -x $DATOS/modes/$PREFIJO.mode ]
@@ -91,7 +91,9 @@ function translate_docx
   OTRASALIDA=/tmp/$$docxsalida.zip
   
   unzip -q -o -d $INPUT_TMPDIR $FICHERO
-  find $INPUT_TMPDIR | grep \\\(document\\\|core\\\|foot.+\\\|head.+\\\|.+notes\\\)\\\.xml |\
+#  find $INPUT_TMPDIR | grep \\\(document\\\|core\\\)\\\.xml |\
+  find $INPUT_TMPDIR | grep "word" | grep "xml" |\
+  grep -v -i \\\(settings\\\|theme\\\|styles\\\|font\\\|rels\\\) |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   $APERTIUM_PATH/apertium-deswxml |\
   if [ ! -x $DATOS/modes/$PREFIJO.mode ]
