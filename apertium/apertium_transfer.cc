b7728f8850a046a9262d7d574863be00d7bade17
Sergio Ortiz Rojas <sergio.ortiz@gmail.com>
2009-03-10 18:32:52 +0000

    transfer bug with -c solved
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@9027 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/apertium_transfer.cc
+++ apertium/apertium_transfer.cc
@@ -26,6 +26,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <apertium/string_utils.h>
+#include <getopt.h>
 #ifdef WIN32
 #include <io.h>
 #include <fcntl.h>
@@ -38,103 +39,175 @@ void message(char *progname)
 {
   cerr << "USAGE: " << basename(progname) << " trules preproc biltrans [input [output]]" << endl;
   cerr << "       " << basename(progname) << " -n trules preproc [input [output]]" << endl;
-  cerr << "       " << basename(progname) << " -x trules preproc biltrans extended [input [output]]" << endl;
-  cerr << "       " << basename(progname) << " -c trules preproc biltrans extended [input [output]]" << endl;
+  cerr << "       " << basename(progname) << " -x extended trules preproc biltrans [input [output]]" << endl;
+  cerr << "       " << basename(progname) << " -c trules preproc biltrans [input [output]]" << endl;
   cerr << "  trules     transfer rules file" << endl;
   cerr << "  preproc    result of preprocess trules file" << endl;
   cerr << "  biltrans   bilingual letter transducer file" << endl;
   cerr << "  input      input file, standard input by default" << endl;
   cerr << "  output     output file, standard output by default" << endl;
   cerr << "  -n         don't use bilingual dictionary" << endl;
-  cerr << "  -x         extended mode with user dictionary" << endl;
+  cerr << "  -x bindix  extended mode with user dictionary" << endl;
   cerr << "  -c         case-sensitiveness while accessing bilingual dictionary" << endl;
   
 
   exit(EXIT_FAILURE);
 }
 
+void testfile(string const &filename)
+{
+  struct stat mybuf;
+  if(stat(filename.c_str(), &mybuf) == -1)
+  {
+    cerr << "Error: can't stat file '";
+    cerr << filename << "'." << endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+FILE * open_input(string const &filename)
+{
+  FILE *input = fopen(filename.c_str(), "r");
+  if(!input)
+  {
+    cerr << "Error: can't open input file '";
+    cerr << filename.c_str() << "'." << endl;
+    exit(EXIT_FAILURE);
+  }
+  
+  return input;
+}  
+
+FILE * open_output(string const &filename)
+{
+  FILE *output = fopen(filename.c_str(), "w");
+  if(!output)
+  {
+    cerr << "Error: can't open output file '";
+    cerr << filename.c_str() << "'." << endl;
+    exit(EXIT_FAILURE);
+  }
+  return output;
+}
+
 int main(int argc, char *argv[])
 {
   LtLocale::tryToSetLocale();
-  
-  if(argc > 7 || argc <4)
-  {
-    message(argv[0]);
-  }
 
-  int firstfile = 1;
-  if(!strcmp(argv[1],"-n") || !strcmp(argv[1], "-x") || !strcmp(argv[1], "-c"))
-  {
-    firstfile = 2;
-  }
-  
-  for(int i = firstfile; i < argc; i++)
-  {
-    struct stat mybuf;
-    if(stat(argv[i], &mybuf) == -1)
-    {
-      cerr << "Error: can't stat file '";
-      cerr << argv[i] << "'." << endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-  
-  FILE *input = stdin, *output = stdout;
-  int argclimit1 = 5;
-  int argclimit2 = 6;
-  
-  if(!strcmp(argv[1],"-x"))
-  {
-    argclimit1 = 7;
-    argclimit2 = 8;
-  }
-  
-  if(argc >= argclimit1)
-  {
-    input = fopen(argv[argclimit1 - 1], "r");
-    if(!input)
-    {
-      cerr << "Error: can't open input file '" << argv[argclimit1 - 1] << "'." << endl;
-      exit(EXIT_FAILURE);
-    }
-    if(argc == argclimit2)
-    {
-      output = fopen(argv[argclimit2 - 1], "w");
-      if(!output)
-      {
-	cerr << "Error: can't open output file '";
-	cerr << argv[argclimit2 - 1] << "'." << endl;
-	exit(EXIT_FAILURE);
-      }
-    }
-  }
-
-  #ifdef WIN32
+#ifdef WIN32
   _setmode(_fileno(input), _O_U8TEXT);
   _setmode(_fileno(output), _O_U8TEXT);
 #endif 
-
+  
   Transfer t;
-  if(firstfile == 1)
-  {
-    t.read(argv[1], argv[2], argv[3]);
+  
+#if HAVE_GETOPT_LONG
+  int option_index=0;
+#endif
+
+  while (true) {
+#if HAVE_GETOPT_LONG
+    static struct option long_options[] =
+    {
+      {"no-bilingual",      no_argument, 0, 'n'},
+      {"extended",      required_argument, 0, 'x'},
+      {"case-sensitive", required_argument, 0, 'c'},
+      {0, 0, 0, 0}
+    };
+
+    int c=getopt_long(argc, argv, "nx:c", long_options, &option_index);
+#else
+    int c=getopt(argc, argv, "nx:c");
+#endif
+    if (c==-1)
+      break;
+      
+    switch (c)
+    {
+      case 'n':
+        t.setUseBilingual(false);
+        break;
+        
+      case 'x':
+        t.setExtendedDictionary(optarg);
+        break;
+        
+      case 'c':
+        t.setCaseSensitiveness(true);
+        break;
+
+      default:
+        message(argv[0]);
+        break;
+    }    
   }
-  else if(!strcmp(argv[1], "-n"))
+
+  FILE *input = stdin, *output = stdout;
+
+  switch(argc - optind + 1)
   {
-    t.read(argv[2], argv[3]);
-    t.setUseBilingual(false);
-  }
-  else if(!strcmp(argv[1], "-c"))
-  {
-    t.read(argv[2], argv[3], argv[4]);
-    t.setCaseSensitiveness(true);
-  }
-  else if(!strcmp(argv[1], "-x"))
-  {
-    t.read(argv[2], argv[3], argv[4]);
-    t.setExtendedDictionary(argv[5]);
-  }
+    case 6:
+      output = open_output(argv[argc-1]);
+      input = open_input(argv[argc-2]);
+      testfile(argv[argc-3]);
+      testfile(argv[argc-4]);
+      testfile(argv[argc-5]);
+      t.read(argv[argc-5], argv[argc-4], argv[argc-3]);
+      break;
+      
+    case 5:
+      if(t.getUseBilingual() == false)
+      {
+        output = open_output(argv[argc-1]);
+        input = open_input(argv[argc-2]);
+        testfile(argv[argc-3]);
+        testfile(argv[argc-4]);
+        t.read(argv[argc-4], argv[argc-3]);
+      }
+      else
+      {
+        input = open_input(argv[argc-1]);
+        testfile(argv[argc-2]);
+        testfile(argv[argc-3]);
+        testfile(argv[argc-4]);
+        t.read(argv[argc-4], argv[argc-3], argv[argc-2]);
+      }
+      break;
+      
+    case 4:
+      if(t.getUseBilingual() == false)
+      {
+        input = open_input(argv[argc-1]);
+        testfile(argv[argc-2]);
+        testfile(argv[argc-3]);
+        t.read(argv[argc-3], argv[argc-2]);
+      }
+      else
+      {
+        testfile(argv[argc-1]);
+        testfile(argv[argc-2]);
+        testfile(argv[argc-3]);
+        t.read(argv[argc-3], argv[argc-2], argv[argc-1]);
+      }
+      break;
+    case 3:
+      if(t.getUseBilingual() == false)
+      {
+        testfile(argv[argc-2]);
+        testfile(argv[argc-3]);
+        t.read(argv[argc-3], argv[argc-2]);
+      }
+      else
+      {
+        message(argv[0]);
+      }
+      break;
+    
+    default:
+      message(argv[0]);
+      break;
+  }  
   
   t.transfer(input, output);
-  return EXIT_SUCCESS;
+  return EXIT_SUCCESS; 
 }
