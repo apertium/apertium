b63a61800eff7379ce87426fe863d17dcc2711f2
Pasquale Minervini <p.minervini@gmail.com>
2009-12-02 14:20:34 +0000

    more checks for lacking _unlocked functions and copyright informations
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@17216 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/unlocked_cstdio.h
+++ apertium/unlocked_cstdio.h
@@ -1,22 +1,57 @@
+/*
+ * Copyright (C) 2005 Universitat d'Alacant / Universidad de Alicante
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
 #ifndef _APERTIUM_UNLOCKED_CSTDIO_
 #define _APERTIUM_UNLOCKED_CSTDIO_
 
 #include <cstdio>
 
 #if !HAVE_DECL_FPUTS_UNLOCKED
-#define fputws_unlocked fputws
+#define fputs_unlocked fputs
 #endif
 
 #if !HAVE_DECL_FGETC_UNLOCKED 
-#define fgetwc_unlocked fgetwc
+#define fgetc_unlocked fgetc
 #endif
 
-#if !HAVE_DECL_PUTC_UNLOCKED
-#define fputwc_unlocked fputwc
+#if !HAVE_DECL_FPUTC_UNLOCKED
+#define fputc_unlocked fputc
+#endif
+
+#if !HAVE_DECL_FWRITE_UNLOCKED
+#define fwrite_unlocked fwrite
 #endif
 
 #if !HAVE_DECL_FREAD_UNLOCKED
 #define fread_unlocked fread
+#endif
+
+#if !HAVE_DECL_FGETWC_UNLOCKED
+#define fgetwc_unlocked fgetwc
+#endif
+
+#if !HAVE_DECL_FPUTWC_UNLOCKED
+#define fputwc_unlocked fputwc
+#endif
+
+#if !HAVE_DECL_FPUTWS_UNLOCKED
+#define fputws_unlocked fputws
 #endif
 
 #endif
