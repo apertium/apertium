478f1179e2893cb17901df027b58004693cb4502
Sergio Ortiz Rojas <sergio.ortiz@gmail.com>
2012-05-31 12:19:45 +0000

    a ver que pasa
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@38533 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/apertium-header.sh
+++ apertium/apertium-header.sh
@@ -161,7 +161,7 @@ translate_odt ()
   fi | \
   if [ ! -x $DATOS/modes/$PREFIJO.mode ]
   then sh $DATOS/modes/$PREFIJO.mode $OPTION $OPTION_TAGGER
-  else $DATOS/modes/$PREFIJO.mode $OPTION $OPTION_TAGGER	 
+  else $DATOS/modes/$PREFIJO.mode $OPTION $OPTION_TAGGER
   fi | \
   $APERTIUM_PATH/apertium-reodt|\
   awk '{punto = index($0, "<?"); cabeza = substr($0, 1, punto-1); cola = substr($0, punto); n1 = substr(cabeza, index(cabeza, "\"")+1); name = substr(n1, 1, index(n1, "\"")-1); gsub("[?]> ", "?>\n", cola); print cola > name;}'
