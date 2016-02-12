4d82f7ea51bb7ed0d3de0a553c31f47ef1fbbde8
Matthew <matthew.marting@earthlink.net>
2016-02-12 18:09:49 +0000

    install an empty tagger.h
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@65571 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/tagger.h
+++ apertium/tagger.h
@@ -1,81 +1,21 @@
-/*
- * Copyright (C) 2005 Universitat d'Alacant / Universidad de Alicante
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- */
-#ifndef __TAGGER_
-#define __TAGGER_
+// Copyright (C) 2005 Universitat d'Alacant / Universidad de Alicante
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-#include <cstdio>
-#include <fstream>
-#include <map>
-#include <set>
+// This header is kept for backwards compatibility with apertium-lex-tools 's
+// tagger_output_processor.h , which includes, but does not use, it.
 
-#include <apertium/constant_manager.h>
-#include <apertium/tagger_data_hmm.h>
-#include <apertium/tagger_data_lsw.h>
-#include <apertium/ttag.h>
-
-using namespace std;
-
-class Tagger
-{
-private:
-
-  enum Mode{UNKNOWN_MODE,
-            TRAIN_HMM_UNSUPERVISED_MODE,
-            TRAIN_HMM_SUPERVISED_MODE,
-            RETRAIN_HMM_UNSUPERVISED_MODE,
-            RETRAIN_HMM_SUPERVISED_MODE,
-            TAGGER_HMM_MODE,
-            TAGGER_HMM_EVAL_MODE,
-            TAGGER_HMM_FIRST_MODE,
-            TRAIN_LSW_UNSUPERVISED_MODE,
-            TRAIN_LSW_SUPERVISED_MODE,
-            RETRAIN_LSW_UNSUPERVISED_MODE,
-            RETRAIN_LSW_SUPERVISED_MODE,
-            TAGGER_LSW_MODE,
-            TAGGER_LSW_EVAL_MODE,
-            TAGGER_LSW_FIRST_MODE};
-            
-  vector<string> filenames;
-  int nit;
-  string name;
-  bool debug;
-  
-  bool showSF; // show superficial forms
-  bool null_flush; // flush on '\0'
-  bool is_sw; // use Sliding-Window algorithm, other than HMM
-  
-  void setShowSF(bool val);
-  bool getShowSF();
-
-  int getMode(int argc, char *argv[]);
-  void taggerHMM(bool model_first=false);
-  void taggerLSW(bool model_first=false);
-  void trainHMM();
-  void trainLSW();
-  void retrainHMM();
-  void retrainLSW();
-  void trainHMMSupervised();
-  void trainLSWSupervised();
-  void help();
-  void filerror(string const &filename);  
-  bool isNumber(const char *str);
-public:
-  Tagger();
-  void main(int argc, char *argv[]);
-};
-
-#endif
+#ifndef TAGGER_H
+#define TAGGER_H
+#endif // TAGGER_H
