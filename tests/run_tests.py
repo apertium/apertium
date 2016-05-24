1cef6c98a998e383dc7cb624febe9a6efaaf0e83
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2016-05-24 09:03:51 +0000

    remove debug output
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@68773 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -11,9 +11,7 @@ if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
     failures = 0
     for module in [pretransfer]:
-        print("pretransfer")
         suite = unittest.TestLoader().loadTestsFromModule(module)
-        print(suite, module)
         res = unittest.TextTestRunner(verbosity = 2).run(suite)
         failures += len(res.failures)
     sys.exit(min(failures, 255))
