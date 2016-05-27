1aa5e19638e82386acbd10c186d884d8aac0affb
Matthew <matthew.marting@earthlink.net>
2016-05-27 03:05:05 +0000

    Add externally-linked template function serialise
    serialise calls the static member function serialise of the Serialiser class of the serialised type.
    
    Use this function to implement all the static member functions
    
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@68880 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/serialiser.h
+++ apertium/serialiser.h
@@ -54,36 +54,36 @@ template <typename SerialisedType> class Serialiser;
 
 template <> class Serialiser<a> {
 public:
-  inline static void serialise(const a &StreamedType_, std::ostream &Stream_);
+  inline static void serialise(const a &SerialisedType_, std::ostream &Output);
 };
 
 template <> class Serialiser<Analysis> {
 public:
   inline static void serialise(const Analysis &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
 
 template <> class Serialiser<i> {
 public:
-  inline static void serialise(const i &StreamedType_, std::ostream &Stream_);
+  inline static void serialise(const i &SerialisedType_, std::ostream &Output);
 };
 
 template <> class Serialiser<Lemma> {
 public:
-  inline static void serialise(const Lemma &StreamedType_,
-                               std::ostream &Stream_);
+  inline static void serialise(const Lemma &SerialisedType_,
+                               std::ostream &Output);
 };
 
 template <> class Serialiser<Morpheme> {
 public:
   inline static void serialise(const Morpheme &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
 
 template <> class Serialiser<Tag> {
 public:
   inline static void serialise(const Tag &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
 
 template <typename value_type>
@@ -91,7 +91,7 @@ class Serialiser<std::basic_string<value_type> > {
 public:
   inline static void
   serialise(const std::basic_string<value_type> &SerialisedType_,
-            std::ostream &Stream_);
+            std::ostream &Output);
 };
 
 template <typename key_type, typename mapped_type>
@@ -99,7 +99,7 @@ class Serialiser<std::map<key_type, mapped_type> > {
 public:
   inline static void
   serialise(const std::map<key_type, mapped_type> &SerialisedType_,
-            std::ostream &Stream_);
+            std::ostream &Output);
 };
 
 template <typename first_type, typename second_type>
@@ -107,79 +107,86 @@ class Serialiser<std::pair<first_type, second_type> > {
 public:
   inline static void
   serialise(const std::pair<first_type, second_type> &SerialisedType_,
-            std::ostream &Stream_);
+            std::ostream &Output);
 };
 
 template <> class Serialiser<std::size_t> {
 public:
   inline static void serialise(const std::size_t &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
 
 template <typename value_type> class Serialiser<std::vector<value_type> > {
 public:
   inline static void serialise(const std::vector<value_type> &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
 
 template <> class Serialiser<wchar_t> {
 public:
   inline static void serialise(const wchar_t &SerialisedType_,
-                               std::ostream &Stream_);
+                               std::ostream &Output);
 };
+}
 
-void Serialiser<a>::serialise(const a &StreamedType_, std::ostream &Stream_) {
-  Serialiser<std::vector<Tag> >::serialise(StreamedType_.TheTags, Stream_);
-  Serialiser<std::vector<Morpheme> >::serialise(StreamedType_.TheMorphemes,
-                                                Stream_);
+template <typename SerialisedType>
+inline void serialise(const SerialisedType &SerialisedType_,
+                      std::ostream &Output) {
+  Serialiser<SerialisedType>::serialise(SerialisedType_, Output);
+}
+
+void Serialiser<a>::serialise(const a &SerialisedType_, std::ostream &Output) {
+  Serialiser<std::vector<Tag> >::serialise(SerialisedType_.TheTags, Output);
+  Serialiser<std::vector<Morpheme> >::serialise(SerialisedType_.TheMorphemes,
+                                                Output);
 }
 
 void Serialiser<Analysis>::serialise(const Analysis &SerialisedType_,
-                                     std::ostream &Stream_) {
+                                     std::ostream &Output) {
   Serialiser<std::vector<Morpheme> >::serialise(SerialisedType_.TheMorphemes,
-                                                Stream_);
+                                                Output);
 }
 
-void Serialiser<i>::serialise(const i &StreamedType_, std::ostream &Stream_) {
-  Serialiser<std::vector<Tag> >::serialise(StreamedType_.TheTags, Stream_);
+void Serialiser<i>::serialise(const i &SerialisedType_, std::ostream &Output) {
+  Serialiser<std::vector<Tag> >::serialise(SerialisedType_.TheTags, Output);
 }
 
-void Serialiser<Lemma>::serialise(const Lemma &StreamedType_,
-                                  std::ostream &Stream_) {
-  Serialiser<std::wstring>::serialise(StreamedType_.TheLemma, Stream_);
+void Serialiser<Lemma>::serialise(const Lemma &SerialisedType_,
+                                  std::ostream &Output) {
+  serialise(SerialisedType_.TheLemma, Output);
 }
 
 void Serialiser<Morpheme>::serialise(const Morpheme &SerialisedType_,
-                                     std::ostream &Stream_) {
-  Serialiser<std::wstring>::serialise(SerialisedType_.TheLemma, Stream_);
-  Serialiser<std::vector<Tag> >::serialise(SerialisedType_.TheTags, Stream_);
+                                     std::ostream &Output) {
+  serialise(SerialisedType_.TheLemma, Output);
+  Serialiser<std::vector<Tag> >::serialise(SerialisedType_.TheTags, Output);
 }
 
 void Serialiser<Tag>::serialise(const Tag &SerialisedType_,
-                                std::ostream &Stream_) {
-  Serialiser<std::wstring>::serialise(SerialisedType_.TheTag, Stream_);
+                                std::ostream &Output) {
+  serialise(SerialisedType_.TheTag, Output);
 }
 
 template <typename value_type>
 void Serialiser<std::basic_string<value_type> >::serialise(
     const std::basic_string<value_type> &SerialisedType_,
-    std::ostream &Stream_) {
-  Serialiser<std::size_t>::serialise(SerialisedType_.size(), Stream_);
+    std::ostream &Output) {
+  serialise(SerialisedType_.size(), Output);
 
   for (typename std::basic_string<value_type>::const_iterator
            SerialisedType_iterator = SerialisedType_.begin();
        // Call .end() each iteration to save memory.
        SerialisedType_iterator != SerialisedType_.end();
        ++SerialisedType_iterator) {
-    Serialiser<value_type>::serialise(*SerialisedType_iterator, Stream_);
+    serialise(*SerialisedType_iterator, Output);
   }
 }
 
 template <typename key_type, typename mapped_type>
 void Serialiser<std::map<key_type, mapped_type> >::serialise(
     const std::map<key_type, mapped_type> &SerialisedType_,
-    std::ostream &Stream_) {
-  Serialiser<std::size_t>::serialise(SerialisedType_.size(), Stream_);
+    std::ostream &Output) {
+  serialise(SerialisedType_.size(), Output);
 
   for (typename std::map<key_type, mapped_type>::const_iterator
            SerialisedType_iterator = SerialisedType_.begin();
@@ -187,24 +194,24 @@ void Serialiser<std::map<key_type, mapped_type> >::serialise(
        SerialisedType_iterator != SerialisedType_.end();
        ++SerialisedType_iterator) {
     Serialiser<std::pair<key_type, mapped_type> >::serialise(
-        *SerialisedType_iterator, Stream_);
+        *SerialisedType_iterator, Output);
   }
 }
 
 template <typename first_type, typename second_type>
 void Serialiser<std::pair<first_type, second_type> >::serialise(
     const std::pair<first_type, second_type> &SerialisedType_,
-    std::ostream &Stream_) {
-  Serialiser<first_type>::serialise(SerialisedType_.first, Stream_);
-  Serialiser<second_type>::serialise(SerialisedType_.second, Stream_);
+    std::ostream &Output) {
+  serialise(SerialisedType_.first, Output);
+  serialise(SerialisedType_.second, Output);
 }
 
 void Serialiser<std::size_t>::serialise(const std::size_t &SerialisedType_,
-                                        std::ostream &Stream_) {
+                                        std::ostream &Output) {
   try {
-    Stream_.put(compressedSize(SerialisedType_));
+    Output.put(compressedSize(SerialisedType_));
 
-    if (!Stream_) {
+    if (!Output) {
       std::stringstream what_;
       what_ << "can't serialise size " << std::hex
             << /* [1] */ +compressedSize(SerialisedType_) << std::dec;
@@ -212,10 +219,10 @@ void Serialiser<std::size_t>::serialise(const std::size_t &SerialisedType_,
     }
 
     for (unsigned char CompressedSize = compressedSize(SerialisedType_);
-         CompressedSize != 0; Stream_.put(static_cast<unsigned char>(
+         CompressedSize != 0; Output.put(static_cast<unsigned char>(
              SerialisedType_ >>
              std::numeric_limits<unsigned char>::digits * --CompressedSize))) {
-      if (!Stream_) {
+      if (!Output) {
         std::stringstream what_;
         what_ << "can't serialise byte " << std::hex
               << /* [1] */ +static_cast<unsigned char>(
@@ -235,23 +242,23 @@ void Serialiser<std::size_t>::serialise(const std::size_t &SerialisedType_,
 
 template <typename value_type>
 void Serialiser<std::vector<value_type> >::serialise(
-    const std::vector<value_type> &SerialisedType_, std::ostream &Stream_) {
-  Serialiser<std::size_t>::serialise(SerialisedType_.size(), Stream_);
+    const std::vector<value_type> &SerialisedType_, std::ostream &Output) {
+  serialise(SerialisedType_.size(), Output);
 
   for (typename std::vector<value_type>::const_iterator value_type_ =
            SerialisedType_.begin();
        // Call .end() each iteration to save memory.
        value_type_ != SerialisedType_.end(); ++value_type_) {
-    Serialiser<value_type>::serialise(*value_type_, Stream_);
+    serialise(*value_type_, Output);
   }
 }
 
 void Serialiser<wchar_t>::serialise(const wchar_t &SerialisedType_,
-                                    std::ostream &Stream_) {
+                                    std::ostream &Output) {
   try {
-    Stream_.put(compressedSize(SerialisedType_));
+    Output.put(compressedSize(SerialisedType_));
 
-    if (!Stream_) {
+    if (!Output) {
       std::stringstream what_;
       what_ << "can't serialise size " << std::hex
             << /* [1] */ +compressedSize(SerialisedType_);
@@ -259,10 +266,10 @@ void Serialiser<wchar_t>::serialise(const wchar_t &SerialisedType_,
     }
 
     for (unsigned char CompressedSize = compressedSize(SerialisedType_);
-         CompressedSize != 0; Stream_.put(static_cast<unsigned char>(
+         CompressedSize != 0; Output.put(static_cast<unsigned char>(
              static_cast<unsigned wchar_t>(SerialisedType_) >>
              std::numeric_limits<unsigned char>::digits * --CompressedSize))) {
-      if (!Stream_) {
+      if (!Output) {
         std::stringstream what_;
         what_ << "can't serialise byte " << std::hex
               << /* [1] */ +(static_cast<unsigned wchar_t>(SerialisedType_) >>
@@ -277,7 +284,6 @@ void Serialiser<wchar_t>::serialise(const wchar_t &SerialisedType_,
           << basic_ExceptionType_.what();
     throw Exception::Serialiser::wchar_t_(what_);
   }
-}
 }
 }
 
