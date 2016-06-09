5867511e081b0ded47180e5ee5288c22d2f0d9b5
Frankie Robertson <frankie@robertson.name>
2016-06-09 14:20:01 +0000

    * Change find_similar_ambiguity_class to pick a superset * Adjust HMM tagging to only use the obtained, more general, ambiguity class for getting the output probability * Adjust LSWPoST tagging to not use find_similar_ambiguity_class at all
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium@69617 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium/hmm.cc
+++ apertium/hmm.cc
@@ -710,7 +710,7 @@ HMM::tagger(FILE *Input, FILE *Output, const bool &First) {
   TaggerWord *word=NULL;
   TTag tag;
   
-  set <TTag> tags, pretags;
+  set <TTag> ambg_class_tags, tags, pretags;
   set <TTag>::iterator itag, jtag;
   
   double prob, loli, x;
@@ -750,9 +750,9 @@ HMM::tagger(FILE *Input, FILE *Output, const bool &First) {
     if (tags.size()==0) // This is an unknown word
       tags = tdhmm.getOpenClass();
                        
-    tags = require_similar_ambiguity_class(tdhmm, tags, *word, debug);
+    ambg_class_tags = require_similar_ambiguity_class(tdhmm, tags, *word, debug);
          
-    k = output[tags];  //Ambiguity class the word belongs to
+    k = output[ambg_class_tags];  //Ambiguity class the word belongs to
     
 #ifdef __GNUC__
     clear_array_double(alpha[nwpend%2], N);    
