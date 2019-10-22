apertium/apertium

commit 605540d934d698bb71d0d09e06eb8bf4f5ffba48

fix SC1117 and find-usage
and add shebang so shellcheck knows what standard to use in source file

apertium/apertium-header.sh
@@ -141,7 +141,7 @@ translate_odt ()
   OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
-  find "$INPUT_TMPDIR" | grep "content\\.xml\\|styles\\.xml" |\
+  find . -name content.xml -o -name styles.xml |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-desodt" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];
@@ -190,15 +190,15 @@ translate_docx ()
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
 
-  for i in $(find "$INPUT_TMPDIR"|grep "xlsx$");
-  do LOCALTEMP=$(mktemp "$TMPDIR/apertium.XXXXXXXX");
+  find "$INPUT_TMPDIR" -name "*.xlsx" -print0 | while read -r -d '' i; do
+    LOCALTEMP=$(mktemp "$TMPDIR/apertium.XXXXXXXX");
     "$APERTIUM_PATH/apertium" -f xlsx -d "$DATADIR" "$OPCIONU" "$PAIR" <"$i" >"$LOCALTEMP";
     cp "$LOCALTEMP" "$i";
     rm "$LOCALTEMP";
   done;
 
-  find "$INPUT_TMPDIR" | grep "xml" |\
-  grep -v -i \\\(settings\\\|theme\\\|styles\\\|font\\\|rels\\\|docProps\\\) |\
+  find "$INPUT_TMPDIR" -name "*.xml" |\
+  grep -E -v -i '(settings|theme|styles|font|rels|docProps)' |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-deswxml" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];
@@ -246,15 +246,14 @@ translate_pptx ()
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
 
-  for i in $(find "$INPUT_TMPDIR"|grep "xlsx$"); do
+  find "$INPUT_TMPDIR" -name "*.xlsx" -print0 | while read -r -d '' i; do
     LOCALTEMP=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
     "$APERTIUM_PATH/apertium" -f xlsx -d "$DATADIR" "$OPCIONU" "$PAIR" <"$i" >"$LOCALTEMP";
     cp "$LOCALTEMP" "$i"
     rm "$LOCALTEMP"
   done;
 
-  find "$INPUT_TMPDIR" | grep "xml$" |\
-  grep "slides\\/slide" |\
+  find . -path '**/slides/slide*.xml' |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-despptx" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];
@@ -297,7 +296,7 @@ translate_xlsx ()
   OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
-  find "$INPUT_TMPDIR" | grep "sharedStrings.xml" |\
+  find "$INPUT_TMPDIR" -name "sharedStrings.xml" |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-desxlsx" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];
@@ -460,7 +459,7 @@ fi
 
 if [[ ! -e "$DATADIR/modes/$PAIR.mode" ]]; then
   echo -n "Error: Mode $PAIR does not exist"
-  c=$(find "$DATADIR/modes"|wc -l)
+  c=$(find "$DATADIR/modes" -name '*.mode' | wc -l)
   if [ "$c" -le 1 ]; then
     echo "."
   else
@@ -565,7 +564,7 @@ case "$FORMAT" in
   rtfu)
     FORMAT="rtf";
     OPTION="-n";
-    MILOCALE=$(locale -a|grep -i -v "utf\\|^C$\\|^POSIX$"|head -1);
+    MILOCALE=$(locale -a | grep -E -i -v -m1 'utf|^C|^POSIX$')
     if [ "$MILOCALE" = "" ]; then
       echo "Error: Install a ISO-8859-1 compatible locale in your system";
       exit 1;

apertium/apertium-unformat-header.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 INPUT_FILE="/dev/stdin"
 OUTPUT_FILE="/dev/stdout"
 
@@ -117,8 +119,7 @@ unformat_pptx ()
      rm "$LOCALTEMP"
   done;
 
-  find "$INPUT_TMPDIR" | grep "xml$" |\
-  grep "slides\/slide" |\
+  find . -path '**/slides/slide*.xml' |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-despptx" >"$SALIDA"
   rm -Rf "$INPUT_TMPDIR"
@@ -183,7 +184,7 @@ SALIDA=$OUTPUT_FILE;
 
 case "$FORMATADOR" in
         rtf)
-		MILOCALE=$(locale -a|grep -i -v "utf\|^C$\|^POSIX$"|head -1);
+                MILOCALE=$(locale -a | grep -E -i -v -m1 'utf|^C|^POSIX$')
 		if [ "$MILOCALE" = "" ]
 		then echo "Error: Install a ISO-8859-1 compatible locale in your system";
 	             exit 1;

commit 8c9a5b5a2f23d3ea03d2e7358e6def69f20897c7

Fix bug introduced in ODT translation (605540d93)
Introduced in https://github.com/apertium/apertium/commit/605540d934d698bb71d0d09e06eb8bf4f5ffba48#diff-81172e11cabd02a4ab9c27131519d378

apertium/apertium-header.sh
@@ -141,7 +141,7 @@ translate_odt ()
   OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
-  find . -name content.xml -o -name styles.xml |\
+  find "$INPUT_TMPDIR" -name content.xml -o -name styles.xml |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   "$APERTIUM_PATH/apertium-desodt" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];