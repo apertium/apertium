apertium/apertium

commit cefce6819944fa78ffcabc53d812b76600677fab

Docx word borders (closes #47); Now requires ICU; Version bumped

apertium/adapt_docx.cc
@@ -1,0 +1,485 @@
+/*
+ * Copyright (C) 2019 Joan Moratinos Jaume
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+#include <getopt.h>
+#include <iostream>
+#include <string>
+#include <libgen.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <stack>
+#include <vector>
+
+#ifdef _MSC_VER
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+#ifdef _WIN32
+#include "utf8.h"
+#include <utf8_fwrap.h>
+#include <icu.h>
+#else
+#include "utf8/utf8.h"
+#include "unicode/uchar.h"
+#endif
+
+using namespace std;
+
+void endProgram(char *name)
+{
+	cout << basename(name) << ": makes some changes to .xml files from .docx components" << endl;
+	cout << "- moves word boundaries to 'legal' positions" << endl;
+	cout << "Gets input file name from stdin (or from -f), writes to stdout" << endl;
+	cout << "USAGE: " << basename(name) << " [-n] [-p] [-f filename]" << endl;
+	cout << "Options:" << endl;
+#if HAVE_GETOPT_LONG
+	cout << "  -n, --name:    writes \"< file name=\"filename\">\" to output" << endl;
+	cout << "  -f, --file:    gets file name as parameter" << endl;
+	cout << "  -p, --pretty:  outputs xml with a pretty format" << endl;
+#else
+	cout << "  -n:    writes \"< file name=\"filename\">\" to output" << endl;
+	cout << "  -f:    gets file name as parameter" << endl;
+	cout << "  -p:    outputs xml with a pretty format" << endl;
+#endif
+	exit(EXIT_FAILURE);
+}
+
+// Iterator for an xml tree
+// Finds elements with given name and type
+// Can search in embedded elements (if _goDown) or only at root level (siblings)
+class Walker {
+public:
+	Walker(xmlNode *_root, string _name, bool _goDown, xmlElementType _type = xmlElementType::XML_ELEMENT_NODE);
+	xmlNode *getCurrent() { return current; }
+	xmlNode *findNext();
+	static vector<xmlNode*> findAll(xmlNode *root, const char *name, bool goDown, xmlElementType _type = xmlElementType::XML_ELEMENT_NODE);
+private:
+	bool match(xmlNode *node);
+	xmlNode *root, *current;
+	stack<xmlNode*> higher;
+	string name;
+	bool goDown;
+	xmlElementType type;
+};
+
+Walker::Walker(xmlNode *_root, string _name, bool _goDown, xmlElementType _type)
+{
+	root = _root;
+	name = _name;
+	type = _type;
+	goDown = _goDown;
+	if (root)
+		higher.push(root);
+	if (match(root))
+		current = root;
+	else
+		current = findNext();
+}
+
+bool Walker::match(xmlNode *node)
+{
+	return node && (node->type == type) && (xmlStrcmp(node->name, (const xmlChar*)name.c_str()) == 0);
+}
+
+xmlNode *Walker::findNext()
+{
+	while (!higher.empty()) {
+		xmlNode *node;
+		xmlNode *top = higher.top();
+		//string name = (char*)top->name;
+		//cout << name << " " << endl;
+		if (goDown && top->children) {
+			node = top->children;
+			higher.push(node);
+			if (match(node)) {
+				current = node;
+				break;
+			}
+			continue;
+		}
+		while (!higher.empty() && !higher.top()->next)
+			higher.pop();
+		if (higher.empty()) {
+			current = NULL;
+			break;
+		}
+		node = higher.top()->next;
+		higher.pop();
+		higher.push(node);
+		if (match(node)) {
+			current = node;
+			break;
+		}
+	}
+	return current;
+}
+
+/*static*/
+vector<xmlNode*> Walker::findAll(xmlNode *root, const char *name, bool goDown, xmlElementType type)
+{
+	vector<xmlNode*> found;
+	if (root) {
+		Walker walker(root, name, goDown, type);
+		while (walker.getCurrent()) {
+			found.push_back(walker.getCurrent());
+			walker.findNext();
+		}
+	}
+	return found;
+}
+
+// Text span with same properties
+// A TextRun can have a "t" tag, whose content is a text fragment
+// A TextRun can be the last run in its group; text cannot be moved
+// between a last run and next ones.
+class TextRun {
+public:
+	TextRun(xmlNode *_root);
+	string getText() { return text; }
+	void setText(string _text);
+	bool hasText() { return containsText; }
+	void setLast(bool _is) { last = _is; }
+	bool isLast() { return last; }
+	void remove();
+private:
+	xmlNode *root;
+	xmlNode *text_0;
+	string text;
+	bool containsText;
+	bool last;
+};
+
+TextRun::TextRun(xmlNode *_root)
+{
+	root = _root;
+	text_0 = NULL;
+	last = false;
+	vector<xmlNode*> tt = Walker::findAll(root->children, "t", false);
+	if (tt.size() == 1) {
+		text_0 = tt[0];
+		containsText = true;
+		for (auto it = tt.begin(); it != tt.end(); it++) {
+			Walker texts((*it)->children, "text", false, xmlElementType::XML_TEXT_NODE);
+			while (texts.getCurrent()) {
+				xmlNode *t = texts.getCurrent();
+				if (xmlNodeGetSpacePreserve((xmlNode*)*it) == 1)
+					text.append((const char*)t->content);
+				else {
+					string c((const char*)t->content);
+					int newBegin = 0;
+					int newEnd = c.length() - 1;
+					while (newBegin <= newEnd && c[newBegin] == ' ')
+						++newBegin;
+					while (newEnd > newBegin && c[newEnd] == ' ')
+						--newEnd;
+					text.append(c.substr(newBegin, newEnd - newBegin + 1));
+				}
+				texts.findNext();
+			}
+		}
+	}
+	else
+		containsText = false;
+}
+
+void TextRun::setText(string _text)
+{
+	xmlReplaceNode(text_0->children, xmlNewText((const xmlChar*)_text.c_str()));
+	xmlNodeSetSpacePreserve(text_0, _text.front() == ' ' || _text.back() == ' ' ? 1 : 0);
+}
+
+void TextRun::remove()
+{
+	xmlUnlinkNode(root);
+}
+
+// A list of text runs
+// Only runs than contain text are included in the list
+class Paragraph {
+public:
+	Paragraph(xmlNode *_root);
+	vector<TextRun*> getTextRuns() { return runs; };
+private:
+	xmlNode *root;
+	vector<TextRun*> runs;
+};
+
+Paragraph::Paragraph(xmlNode *_root)
+{
+	root = _root;
+	vector<xmlNode*> nodes = Walker::findAll(root->children, "r", false);
+	for (auto it = nodes.begin(); it != nodes.end(); it++) {
+		TextRun *r = new TextRun(*it);
+		if (r->hasText())
+			runs.push_back(r);
+		else
+			delete r;
+	}
+	if (!runs.empty())
+		runs.back()->setLast(true);
+}
+
+// Show text of paragraphs (for debuggins purposes)
+void showParagraphs(xmlNode *root)
+{
+	vector<xmlNode*> pp = Walker::findAll(root, "p", true);
+	cout << pp.size() << " paragraphs found" << endl;
+	for (auto it1 = pp.begin(); it1 != pp.end(); it1++) {
+		Paragraph p(*it1);
+		bool first = true;
+		vector<TextRun*> rr = p.getTextRuns();
+		for (auto it2 = rr.begin(); it2 != rr.end(); it2++) {
+			if (!first)
+				cout << "|";
+			cout << (*it2)->getText();
+			//(*it2)->setText(" " + (*it2)->getText() + " ");
+			first = false;
+		}
+		cout << endl;
+	}
+}
+
+// Find a suitable boundary at begin position or to the right
+// No boundary is allowed in groups "l·l" and "alpha-alpha"
+// TODO: adapt to source language
+size_t findBoundary(string _text, size_t begin)
+{
+	size_t length = _text.length();
+	if (begin == 0 || begin >= length)
+		return begin;
+	const unsigned char *text = (unsigned char *) _text.c_str();
+	utf8::iterator<const unsigned char*> textBegin(text, text, text + length);
+	utf8::iterator<const unsigned char*> textEnd(text + length, text, text + length);
+	utf8::iterator<const unsigned char*> it(text + begin, text, text + length);
+	utf8::iterator<const unsigned char*> minBoundary = it;
+	uint32_t cur = *it, cur_1 = 0, cur_2 = 0;
+	utf8::iterator<const unsigned char*> it2 = it;
+	cur_1 = *(--it2);
+	if (it2 != textBegin)
+		cur_2 = *(--it2);
+	bool advance, expectL, expectAlpha;
+	while (true) {
+		advance = expectL = expectAlpha = false;
+		if ((u_isalpha(cur_1) && u_isalpha(cur)))					// ?a|a
+			advance = true;
+		else if (u_isalpha(cur_2) && cur_1 == '-')					// a-|?
+			advance = u_isalpha(cur);
+		else if (u_isalpha(cur_1) && cur == '-')					// ?a|-
+			expectAlpha = true;
+		else if ((cur_2 == 'L' || cur_2 == 'l') && cur_1 == 0xB7)	// l·|?
+			advance = (cur == 'L' || cur == 'l');
+		else if ((cur_1 == 'L' || cur_1 == 'l') && cur == 0xB7)		// ?l|·
+			expectL = true;
+		if (advance || expectAlpha || expectL)
+		{
+			cur_2 = cur_1;
+			cur_1 = cur;
+			if (++it == textEnd)
+				break;
+			else
+				cur = *it;
+			if (expectAlpha && !u_isalpha(cur)) {
+				--it;
+				break;
+			}
+			if (expectL && !(cur == 'L' || cur == 'l')) {
+				--it;
+				break;
+			}
+		}
+		else
+			break;
+	}
+	return begin + it.base() - minBoundary.base();
+}
+
+// Adjusts the boundaries between adjacent text runs
+// Eventually removes text runs that have become empty
+void adjust(vector<TextRun*> runs, vector<size_t> lengths, string join)
+{
+	if (runs.size() == 1)
+		return;
+	bool changes = false;
+	size_t left = 0, right = 1;
+	size_t endLeft = lengths[0];
+	while (right < runs.size()) {
+		if (left == right) {
+			++right;
+			continue;
+		}
+		if (lengths[left] == 0) {
+			++left;
+			endLeft += lengths[left];
+			continue;
+		}
+		size_t boundary = findBoundary(join, endLeft);
+		size_t toMove = boundary - endLeft;
+		if (toMove > 0) {
+			changes = true;
+			while (toMove > 0 && right < runs.size()) {
+				if (toMove >= lengths[right]) {
+					lengths[left] += lengths[right];
+					endLeft += lengths[right];
+					toMove -= lengths[right];
+					lengths[right] = 0;
+					++right;
+				}
+				else {
+					lengths[left] += toMove;
+					endLeft += toMove;
+					lengths[right] -= toMove;
+					toMove = 0;
+				}
+			}
+		}
+		++left;
+		endLeft += lengths[left];
+	}
+	if (changes) {
+		int begin = 0;
+		for (size_t i = 0; i < runs.size(); ++i) {
+			if (lengths[i] == 0)
+				runs[i]->remove();
+			else {
+				runs[i]->setText(join.substr(begin, lengths[i]));
+				begin += lengths[i];
+			}
+		}
+	}
+}
+
+// Process the document
+// For each paragraph, finds text runs and adjusts them
+void process(xmlNode *root)
+{
+	//showParagraphs(root);
+	vector<xmlNode*> paragraphs = Walker::findAll(root, "p", true);
+	for (auto itp = paragraphs.begin(); itp != paragraphs.end(); itp++) {
+		Paragraph paragraph(*itp);
+		vector<TextRun*> allRuns = paragraph.getTextRuns();
+		string joinedText;
+		vector<size_t> lengths;
+		vector<TextRun*> adjustRuns;
+		for (auto itr = allRuns.begin(); itr != allRuns.end(); itr++) {
+			TextRun *run = *itr;
+			adjustRuns.push_back(run);
+			lengths.push_back(run->getText().length());
+			joinedText.append(run->getText());
+			if (run->isLast()) {
+				adjust(adjustRuns, lengths, joinedText);
+				adjustRuns.clear();
+				lengths.clear();
+				joinedText.clear();
+			}
+		}
+	}
+	//showParagraphs(root);
+}
+
+// Opens a document, processes it and sends it to stdout
+// Can output the name (<file name="XXX"/>
+// The result can be pretty
+void process(string fileName, bool outputsName, bool pretty)
+{
+	xmlDoc *document = xmlReadFile(fileName.c_str(), NULL, XML_PARSE_NOENT);
+	if (document == NULL) {
+		cerr << "error: could not parse file \"" << fileName << "\"" << endl;
+		return;
+	}
+	process(xmlDocGetRootElement(document));
+	xmlChar *buffer;
+	int sizeBuffer;
+	xmlDocDumpFormatMemory(document, &buffer, &sizeBuffer, pretty ? 1: 0);
+	if (outputsName)
+		cout << "<file name=\"" << fileName << "\"/> ";
+	const unsigned char *p = buffer;
+	while (*p && *p != '\x0A' && *p != '\x0D')
+		cout << *(p++);
+	cout << ' ';
+	while (*p == '\x0A' || *p == '\x0D')
+		++p;
+	cout << p << std::flush;
+ 	xmlFree(buffer);
+	xmlFreeDoc(document);
+}
+
+int main(int argc, char *argv[])
+{
+
+	bool outputsName = false;
+	bool pretty = false;
+	bool argF = false;
+	string name;
+
+#if HAVE_GETOPT_LONG
+	static struct option long_options[] =
+	{
+	  {"name",   0, 0, 'n'},
+	  {"pretty", 0, 0, 'n'},
+	  {"file",   0, 0, 'n'},
+	  {"help",   0, 0, 'h'}
+	};
+#endif
+
+	while (true)
+	{
+#if HAVE_GETOPT_LONG
+		int option_index;
+		int c = getopt_long(argc, argv, "npf:h", long_options, &option_index);
+#else
+		int c = getopt(argc, argv, "npf:h");
+#endif
+
+		if (c == -1)
+		{
+			break;
+		}
+
+		switch (c)
+		{
+		case 'n':
+			outputsName = true;
+			break;
+		case 'p':
+			pretty = true;
+			break;
+		case 'f':
+			argF = true;
+			name = optarg;
+			break;
+		case 'h':
+		default:
+			endProgram(argv[0]);
+			break;
+		}
+	}
+	if (argF) {
+		//cout << "out: " << outputName << ", name: " << name << ", pretty: " << pretty << endl;
+		process(name, outputsName, pretty);
+	}
+	else {
+		while (cin >> name) {
+			//cout << "out: " << outputName << ", name: " << name << ", pretty: " << pretty << endl;
+			process(name, outputsName, pretty);
+		}
+	}
+	cout << '\0' << flush;
+}
+
+/* vim: set noet ts=4 sw=4: */

tests/adaptdocx/__init__.py
@@ -1,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from subprocess import Popen, PIPE
+
+
+class AdaptDocxTest(unittest.TestCase):
+    """
+    Subclass and override inputs/expectedOutputs (and possibly other stuff)
+    to create new adapt-docx tests.
+    """
+
+    def runTest(self):
+        pass
+
+    def extractText(self, xmlFile, adapta):
+        try:
+            if adapta:
+                entrada = (xmlFile + "\n").encode("utf8")
+                cmd = ["../apertium/adapt-docx"]
+                proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                entrada = proc.communicate(entrada, 2)[0]
+            else:
+                cmd = ["cat", xmlFile]
+                proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                entrada = proc.communicate(None, 2)[0]
+            cmd = ["python3", "adaptdocx/extract_docx.py"]
+            proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            result = proc.communicate(entrada, 2)[0]
+            result = result.decode("utf8").split("\n")
+            return result
+        finally:
+            pass
+
+
+class SameTextTest(AdaptDocxTest):
+
+    def runTest(self):
+        for nom in ["docx-01"]:
+            original = [linia.replace("|", "") for linia in self.extractText("data/" + nom + ".xml", False)]
+            adaptat  = [linia.replace("|", "") for linia in self.extractText("data/" + nom + ".xml", True)]
+            self.assertListEqual(original, adaptat)
+
+
+class CorrectBoundariesTest(AdaptDocxTest):
+
+    def runTest(self):
+        for nom in ["docx-01"]:
+            obtingut = self.extractText("data/" + nom + ".xml", True)
+            with open("data/" + nom + ".txt") as f:
+                esperat = [linia.strip("\n") for linia in f.readlines()]
+            self.assertListEqual(obtingut, esperat)
+

.gitignore
@@ -95,6 +95,13 @@ Makefile.in
 /scripts/apertium-translate-to-default-equivalent
 
 # Tests
+/tests/**/Makefile.in
+/tests/**/Makefile
+/tests/**/.libs
+/tests/odt/out.odt
+/tests/odt/run.log
+/tests/odt/run.trs
+
 /tests/tagger/test-find-similar-ambiguity-class
 /tests/data/nno-nob.t3x.bin
 

apertium/Makefile.am
@@ -240,7 +240,8 @@ bin_PROGRAMS = apertium-cleanstream \
 	       apertium-tagger-readwords \
 	       apertium-perceptron-trace \
 	       apertium-tmxbuild \
-	       apertium-transfer
+	       apertium-transfer \
+	       apertium-adapt-docx
 
 bin_SCRIPTS =  $(GENERATEDSCRIPTS) apertium-get/apertium-get
 
@@ -344,11 +345,14 @@ apertium_tagger_readwords_LDADD = $(APERTIUM_LIBS) -lapertium$(GENERIC_MAJOR_VER
 ###apertium_gen_wlist_lextor_translation_SOURCES = apertium_gen_wlist_lextor_translation.cc
 ###apertium_gen_wlist_lextor_translation_LDADD = $(APERTIUM_LIBS) -lapertium$(GENERIC_MAJOR_VERSION) $(lib_LTLIBRARIES)
 
+apertium_adapt_docx_SOURCES = adapt_docx.cc
+apertium_adapt_docx_LDADD = $(APERTIUM_LIBS) -lapertium$(GENERIC_MAJOR_VERSION) $(lib_LTLIBRARIES) $(ICU_LIBS)
+
 
 if WINDOWS
-INCLUDES = -I$(top_srcdir)/utf8 -I$(top_srcdir)/apertium/win32 -I$(top_srcdir) $(APERTIUM_CFLAGS)
+INCLUDES = -I$(top_srcdir)/utf8 -I$(top_srcdir)/apertium/win32 -I$(top_srcdir) $(APERTIUM_CFLAGS) $(ICU_CFLAGS)
 else
-AM_CPPFLAGS = -I$(top_srcdir) $(APERTIUM_CFLAGS)
+AM_CPPFLAGS = -I$(top_srcdir) $(APERTIUM_CFLAGS) $(ICU_CFLAGS)
 endif
 CLEANFILES = *~ apertium_destxt.cc apertium_retxt.cc apertium_deshtml.cc \
              apertium_rehtml.cc apertium_desrtf.cc apertium_rertf.cc \

apertium/apertium-header.sh
@@ -199,7 +199,7 @@ translate_docx ()
 
   find "$INPUT_TMPDIR" -name "*.xml" |\
   grep -E -v -i '(settings|theme|styles|font|rels|docProps)' |\
-  awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
+  "$APERTIUM_PATH/apertium-adapt-docx" -n |\
   "$APERTIUM_PATH/apertium-deswxml" "${FORMAT_OPTIONS[@]}" |\
   if [ "$TRANSLATION_MEMORY_FILE" = "" ];
   then cat;

configure.ac
@@ -1,13 +1,10 @@
-#                                               -*- Autoconf -*-
-# Process this file with autoconf to produce a configure script.
-
 AC_PREREQ(2.52)
 
 m4_define([required_lttoolbox_version], [3.5.0])
 m4_define([required_libxml_version], [2.6.17])
 m4_define([required_libpcre_version], [6.4])
 
-AC_INIT([apertium], [3.6.1], [apertium-stuff@lists.sourceforge.net])
+AC_INIT([apertium], [3.6.2], [apertium-stuff@lists.sourceforge.net])
 AC_CONFIG_HEADER([apertium/apertium_config.h])
 
 AC_CANONICAL_SYSTEM
@@ -17,7 +14,7 @@ GENERIC_LIBRARY_NAME=apertium
 # Release versioning
 GENERIC_MAJOR_VERSION=3
 GENERIC_MINOR_VERSION=6
-GENERIC_MICRO_VERSION=1
+GENERIC_MICRO_VERSION=2
 
 # API version (often = GENERIC_MAJOR_VERSION.GENERIC_MINOR_VERSION)
 GENERIC_API_VERSION=$GENERIC_MAJOR_VERSION.$GENERIC_MINOR_VERSION
@@ -156,6 +153,9 @@ AC_TYPE_SIZE_T
 
 # Checks for library functions.
 AC_FUNC_ERROR_AT_LINE
+
+# Checks for ICU
+AC_CHECK_ICU
 
 AC_CHECK_DECLS([fread_unlocked, fwrite_unlocked, fgetc_unlocked, fputc_unlocked, fputs_unlocked, getopt, getopt_long, fgetwc_unlocked, fputwc_unlocked, fgetws_unlocked, fputws_unlocked])
 AC_CHECK_FUNCS([setlocale strdup getopt snprintf mbtowc])

m4/ax_check_icu.m4
@@ -1,0 +1,117 @@
+# ===========================================================================
+#       https://www.gnu.org/software/autoconf-archive/ax_check_icu.html
+# ===========================================================================
+#
+# SYNOPSIS
+#
+#   AX_CHECK_ICU(version, action-if, action-if-not)
+#
+# DESCRIPTION
+#
+#   Defines ICU_LIBS, ICU_CFLAGS, ICU_CXXFLAGS. See icu-config(1) man page.
+#
+# LICENSE
+#
+#   Copyright (c) 2008 Akos Maroy <darkeye@tyrell.hu>
+#
+#   Copying and distribution of this file, with or without modification, are
+#   permitted in any medium without royalty provided the copyright notice
+#   and this notice are preserved. This file is offered as-is, without any
+#   warranty.
+
+#serial 7
+
+AU_ALIAS([AC_CHECK_ICU], [AX_CHECK_ICU])
+AC_DEFUN([AX_CHECK_ICU], [
+  succeeded=no
+
+  if test -z "$ICU_CONFIG"; then
+    AC_PATH_PROG(ICU_CONFIG, icu-config, no)
+  fi
+
+  if test -z "$PKG_CONFIG"; then
+    AC_PATH_PROG(PKG_CONFIG, pkg-config, no)
+  fi
+
+  if test "$ICU_CONFIG" = "no" && test "$PKG_CONFIG" = "no" ; then
+    echo "*** Neither icu-config nor pkg-config could not be found. Make sure either is"
+    echo "*** in your path, and that taglib is properly installed."
+    echo "*** Or see http://ibm.com/software/globalization/icu/"
+  fi
+
+  if test "$ICU_CONFIG" != "no" ; then
+    ICU_VERSION=`$ICU_CONFIG --version`
+    AC_MSG_CHECKING(for ICU >= $1 via icu-config)
+        VERSION_CHECK=`expr $ICU_VERSION \>\= $1`
+        if test "$VERSION_CHECK" = "1" ; then
+            AC_MSG_RESULT(yes)
+            succeeded=yes
+
+            AC_MSG_CHECKING(ICU_CPPFLAGS)
+            ICU_CPPFLAGS=`$ICU_CONFIG --cppflags`
+            AC_MSG_RESULT($ICU_CPPFLAGS)
+
+            AC_MSG_CHECKING(ICU_CFLAGS)
+            ICU_CFLAGS=`$ICU_CONFIG --cflags`
+            AC_MSG_RESULT($ICU_CFLAGS)
+
+            AC_MSG_CHECKING(ICU_CXXFLAGS)
+            ICU_CXXFLAGS=`$ICU_CONFIG --cxxflags`
+            AC_MSG_RESULT($ICU_CXXFLAGS)
+
+            AC_MSG_CHECKING(ICU_LIBS)
+            ICU_LIBS=`$ICU_CONFIG --ldflags`
+            AC_MSG_RESULT($ICU_LIBS)
+        else
+            ICU_CPPFLAGS=""
+            ICU_CFLAGS=""
+            ICU_CXXFLAGS=""
+            ICU_LIBS=""
+            ## If we have a custom action on failure, don't print errors, but
+            ## do set a variable so people can do so.
+            ifelse([$3], ,echo "can't find ICU >= $1 via icu-config",)
+        fi
+  fi
+
+  if test "$succeeded" != "yes" && test "$PKG_CONFIG" != "no" ; then
+    AC_MSG_CHECKING(for ICU >= $1 via pkg-config)
+        if $PKG_CONFIG --atleast-version=$1 icu-i18n ; then
+            AC_MSG_RESULT(yes)
+            succeeded=yes
+
+            AC_MSG_CHECKING(ICU_CPPFLAGS)
+            ICU_CPPFLAGS=`$PKG_CONFIG --variable=CPPFLAGS icu-i18n`
+            AC_MSG_RESULT($ICU_CPPFLAGS)
+
+            AC_MSG_CHECKING(ICU_CFLAGS)
+            ICU_CFLAGS=`$PKG_CONFIG --variable=CFLAGS icu-i18n`
+            AC_MSG_RESULT($ICU_CFLAGS)
+
+            AC_MSG_CHECKING(ICU_CXXFLAGS)
+            ICU_CXXFLAGS=`$PKG_CONFIG --variable=CXXFLAGS icu-i18n`
+            AC_MSG_RESULT($ICU_CXXFLAGS)
+
+            AC_MSG_CHECKING(ICU_LIBS)
+            ICU_LIBS=`$PKG_CONFIG --libs icu-i18n`
+            AC_MSG_RESULT($ICU_LIBS)
+        else
+            ICU_CPPFLAGS=""
+            ICU_CFLAGS=""
+            ICU_CXXFLAGS=""
+            ICU_LIBS=""
+            ## If we have a custom action on failure, don't print errors, but
+            ## do set a variable so people can do so.
+            ifelse([$3], ,echo "can't find ICU >= $1 via pkg-config",)
+        fi
+  fi
+
+  if test "$succeeded" = "yes"; then
+     AC_SUBST(ICU_CPPFLAGS)
+     AC_SUBST(ICU_CFLAGS)
+     AC_SUBST(ICU_CXXFLAGS)
+     AC_SUBST(ICU_LIBS)
+     ifelse([$2], , :, [$2])
+  else
+     ifelse([$3], , AC_MSG_ERROR([Library requirements (ICU) not met.]), [$3])
+  fi
+])

tests/adaptdocx/extract_docx.py
@@ -1,0 +1,80 @@
+#! /usr/bin/python3
+
+"""
+Extract text from a .xml in a .docx file
+Reads the file from stdin
+"""
+
+import lxml.etree as ET
+import io
+import re
+import os, sys, argparse
+
+
+class docx():
+
+    def __init__(self, linies):
+        self.parser = ET.XMLParser(remove_blank_text=True)
+        self.tot = ET.fromstring("".join(lin.strip() for lin in linies).encode("utf8"), self.parser)
+        self.pars = [paragraf(p) for p in docx.iter(self.tot, "w:p")]
+
+    @staticmethod
+    def iter(element, tag):
+        ns, nom = tag.split(":")
+        for e in element.iter("{%s}%s" % (docx.ns[ns], nom)):
+            yield e
+
+    ns = {
+        "r":   "http://schemas.openxmlformats.org/officeDocument/2006/relationships",
+        "wp":  "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing",
+        "w":   "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
+        "w10": "urn:schemas-microsoft-com:office:word",
+        "w14": "http://schemas.microsoft.com/office/word/2010/wordml",
+        "wps": "http://schemas.microsoft.com/office/word/2010/wordprocessingShape",
+        "wpg": "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup",
+        "mc":  "http://schemas.openxmlformats.org/markup-compatibility/2006",
+        "v":   "urn:schemas-microsoft-com:vml",
+        "o":   "urn:schemas-microsoft-com:office:office",
+        "m":   "http://schemas.openxmlformats.org/officeDocument/2006/math",
+        "xml": "http://www.w3.org/XML/1998/namespace"
+    }
+
+
+
+class paragraf():
+
+    def __init__(self, element):
+        self.element = element
+        self.rr = [r for r in docx.iter(element, "w:r")]
+        self.tt = [t for r in self.rr for t in docx.iter(r, "w:t")]
+        texts = []
+        for t in self.tt:
+            if self.get_atrib(t, "xml:space") == "preserve":
+                texts.append(t.text)
+            else:
+                texts.append(t.text.strip())
+        self.txt = "|".join(texts)
+        self.mides = [len(t) for t in texts]
+
+    def get_atrib(self, element, nom, defecte = None):
+        pn = nom.split(":", 1)
+        pn = "{%s}%s" % (docx.ns[pn[0]], pn[1])
+        valor = element.get(pn, defecte)
+        return valor
+
+
+linia = ""
+while True:
+    c = sys.stdin.read(1)
+    if not c or c == '\0':
+        break
+    else:
+        linia += c
+linies = [ linia ]
+#sys.stdout.write("#línies: %d" % (len(linies)))
+doc = docx(linies)
+for p in doc.pars:
+    print(p.txt)
+sys.stdout.write('\0')
+sys.stdout.flush()
+

commit f9dc2c125f07300dcefa97d7bd85b4cc023232f6

adapt-docx: Never output newlines unless -p (pretty)
Fixes a regression introduced in cefce6819944fa78ffcabc53d812b76600677fab

The awk script in translate_docx etc. that handles turning the stream
back into filenames expects filenames at the start of each line, and
no newlines in the actual contents of each file.

apertium/adapt_docx.cc
@@ -392,6 +392,16 @@ void process(xmlNode *root)
 	//showParagraphs(root);
 }
 
+inline const unsigned char* printNl2spc(const unsigned char *p) {
+	while (*p && *p != '\x0A' && *p != '\x0D')
+		cout << *(p++);
+	cout << ' ';
+	while (*p == '\x0A' || *p == '\x0D')
+		++p;
+	return p;
+}
+
+
 // Opens a document, processes it and sends it to stdout
 // Can output the name (<file name="XXX"/>
 // The result can be pretty
@@ -409,13 +419,15 @@ void process(string fileName, bool outputsName, bool pretty)
 	if (outputsName)
 		cout << "<file name=\"" << fileName << "\"/> ";
 	const unsigned char *p = buffer;
-	while (*p && *p != '\x0A' && *p != '\x0D')
-		cout << *(p++);
-	cout << ' ';
-	while (*p == '\x0A' || *p == '\x0D')
-		++p;
-	cout << p << std::flush;
- 	xmlFree(buffer);
+	p = printNl2spc(p);
+	if (pretty)
+		cout << p;
+	else
+		do
+			p = printNl2spc(p);
+		while (*p);
+	cout << endl;
+        xmlFree(buffer);
 	xmlFreeDoc(document);
 }
 
@@ -470,12 +482,12 @@ int main(int argc, char *argv[])
 		}
 	}
 	if (argF) {
-		//cout << "out: " << outputName << ", name: " << name << ", pretty: " << pretty << endl;
+		// cerr << "out: " << outputsName << ", name: " << name << ", pretty: " << pretty << endl;
 		process(name, outputsName, pretty);
 	}
 	else {
 		while (cin >> name) {
-			//cout << "out: " << outputName << ", name: " << name << ", pretty: " << pretty << endl;
+			// cerr << "out: " << outputsName << ", name: " << name << ", pretty: " << pretty << endl;
 			process(name, outputsName, pretty);
 		}
 	}

tests/adaptdocx/__init__.py
@@ -15,13 +15,16 @@ class AdaptDocxTest(unittest.TestCase):
     def runTest(self):
         pass
 
+    def adapt(self, xmlFile):
+        entrada = (xmlFile + "\n").encode("utf8")
+        cmd = ["../apertium/apertium-adapt-docx"]
+        proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        return proc.communicate(entrada, 2)[0]
+
     def extractText(self, xmlFile, adapta):
         try:
             if adapta:
-                entrada = (xmlFile + "\n").encode("utf8")
-                cmd = ["../apertium/adapt-docx"]
-                proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                entrada = proc.communicate(entrada, 2)[0]
+                entrada = self.adapt(xmlFile)
             else:
                 cmd = ["cat", xmlFile]
                 proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
@@ -53,3 +56,11 @@ class CorrectBoundariesTest(AdaptDocxTest):
                 esperat = [linia.strip("\n") for linia in f.readlines()]
             self.assertListEqual(obtingut, esperat)
 
+
+class NoNewlinesTest(AdaptDocxTest):
+
+    def runTest(self):
+        for nom in ["docx-newlines"]:
+            obtingut = self.adapt("data/" + nom + ".xml")
+            esperat = open("data/" + nom + ".adapted.xml", "rb").read()
+            self.assertEqual(obtingut, esperat)