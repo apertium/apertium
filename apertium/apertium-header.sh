073b408f52e1492216eebda96cc03aa0218c1a01
Tino Didriksen <mail@tinodidriksen.com>
2020-08-23 22:37:00 +0200

    When available, use Transfuse for -f odt, docx, pptx, html; Use [[ ]] instead of [ ]; Wrap apertium-wblank-mode around all modes; Require UTF-8 earlier and always; Clean up copy-paste superfluousness

--- apertium/apertium-header.sh
+++ apertium/apertium-header.sh
@@ -29,6 +29,8 @@ message ()
   echo " -o direction     translation direction using the translation memory,"
   echo "                  by default 'direction' is used instead"
   echo " -l               lists the available translation directions and exits"
+  # This is wrong and should be deprecated, but until all tools consider \0 flush without -z then this helps to have
+  echo " -z               force null-flush mode on all parts of the pipe"
   echo " direction        typically, LANG1-LANG2, but see modes.xml in language data"
   echo " in               input file (stdin by default)"
   echo " out              output file (stdout by default)"
@@ -59,6 +61,22 @@ check_encoding () {
   if [[ "${encoding}" != utf-8 && "${encoding}" != us-ascii ]]; then
     echo "Input seems to be non-UTF-8, please convert to UTF-8 (e.g. with 'iconv -f ${encoding} -t utf-8')" >&2
     exit 1
+  fi
+}
+
+check_transfuse () {
+  local has_tf=`command -v tf-extract`
+  if [[ "x$APERTIUM_TRANSFUSE" = "x1" || "x$APERTIUM_TRANSFUSE" = "xyes" ]]; then
+    if [[ -z "$has_tf" ]]; then
+      echo "APERTIUM_TRANSFUSE=$APERTIUM_TRANSFUSE but couldn't find Transfuse's tf-extract in PATH"
+      exit 1
+    fi
+  fi
+
+  if [[ "x$APERTIUM_TRANSFUSE" = "x0" || "x$APERTIUM_TRANSFUSE" = "xno" || -z "$has_tf" ]]; then
+    USE_TRANSFUSE=no
+  elif [[ "x$APERTIUM_TRANSFUSE" = "x1" || "x$APERTIUM_TRANSFUSE" = "xyes" || ! -z "$has_tf" ]]; then
+    USE_TRANSFUSE=yes
   fi
 }
 
@@ -93,25 +111,21 @@ translate_latex()
     BORRAFICHERO="true"
   fi
   check_encoding "${INFILE}"
-  locale_utf8
 
   set -o pipefail
   apertium-prelatex "$INFILE" | \
     apertium-utils-fixlatex | \
     apertium-deslatex "${FORMAT_OPTIONS[@]}" | \
-    if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+    if [[ "$TRANSLATION_MEMORY_FILE" = "" ]];
     then cat;
     else lt-tmxproc "$TMCOMPFILE";
     fi | \
-      if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-      sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-    else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-    fi | \
+      bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
       apertium-relatex| \
       awk '{gsub("</CONTENTS-noeos>", "</CONTENTS>"); print;}' | \
-      if [ "$REDIR" == "" ]; then apertium-postlatex-raw; else apertium-postlatex-raw > "$SALIDA"; fi
+      if [[ -z "$REDIR" ]]; then apertium-postlatex-raw; else apertium-postlatex-raw > "$OUT_FILE"; fi
 
-    if [ "$BORRAFICHERO" = "true" ]; then
+    if [[ "$BORRAFICHERO" = "true" ]]; then
       rm -Rf "$INFILE"
     fi
 }
@@ -125,64 +139,69 @@ translate_latex_raw()
 }
 
 
-translate_odt ()
-{
+translate_odt () {
+  check_transfuse
+  if [[ $USE_TRANSFUSE = "yes" ]]; then
+    tf-extract -f odt | bash <(apertium-wblank-mode -z "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then tf-inject; else tf-inject > "$OUT_FILE"; fi
+    return
+  fi
+
   INPUT_TMPDIR=$(mktemp -d "$TMPDIR/apertium.XXXXXXXX")
 
-  locale_utf8
   test_zip
 
-  if [ "$INFILE" = "" ]; then
+  if [[ -z "$INFILE" ]]; then
     INFILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
     cat > "$INFILE"
     BORRAFICHERO="true"
   fi
-  OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
+  TMP_OUT_FILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
   find "$INPUT_TMPDIR" -name content.xml -o -name styles.xml |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   apertium-desodt "${FORMAT_OPTIONS[@]}" |\
-  if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+  if [[ -z "$TRANSLATION_MEMORY_FILE" ]];
   then cat;
   else lt-tmxproc "$TMCOMPFILE";
   fi | \
-    if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-    sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | \
+    bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
     apertium-reodt|\
   awk '{punto = index($0, "/>") + 3; cabeza = substr($0, 1, punto-1); cola = substr($0, punto); n1 = substr(cabeza, index(cabeza, "\"")+1); name = substr(n1, 1, index(n1, "\"")-1); gsub("[?]> ", "?>\n", cola); print cola > name;}'
   VUELVE=$(pwd)
   cd "$INPUT_TMPDIR"
   rm -Rf ObjectReplacements
-  zip -q -r - . >"$OTRASALIDA"
+  zip -q -r - . >"$TMP_OUT_FILE"
   cd "$VUELVE"
   rm -Rf "$INPUT_TMPDIR"
 
-  if [ "$BORRAFICHERO" = "true" ]; then
+  if [[ "$BORRAFICHERO" = "true" ]]; then
     rm -Rf "$INFILE";
   fi
 
-  if [ "$REDIR" == "" ]; then cat "$OTRASALIDA"; else cat "$OTRASALIDA" > "$SALIDA"; fi
-  rm -Rf "$OTRASALIDA"
+  if [[ -z "$REDIR" ]]; then cat "$TMP_OUT_FILE"; else cat "$TMP_OUT_FILE" > "$OUT_FILE"; fi
+  rm -Rf "$TMP_OUT_FILE"
 }
 
-translate_docx ()
-{
+translate_docx () {
+  check_transfuse
+  if [[ $USE_TRANSFUSE = "yes" ]]; then
+    tf-extract -f docx | bash <(apertium-wblank-mode -z "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then tf-inject; else tf-inject > "$OUT_FILE"; fi
+    return
+  fi
+
   INPUT_TMPDIR=$(mktemp -d "$TMPDIR/apertium.XXXXXXXX")
 
-  locale_utf8
   test_zip
 
-  if [ "$INFILE" = "" ]; then
+  if [[ -z "$INFILE" ]]; then
     INFILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
     cat > "$INFILE"
     BORRAFICHERO="true"
   fi
-  OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
+  TMP_OUT_FILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
-  if [ "$UWORDS" = "no" ]; then
+  if [[ "$UWORDS" = "no" ]]; then
     OPCIONU="-u";
   else OPCIONU="";
   fi
@@ -200,45 +219,46 @@ translate_docx ()
   grep -E -v -i '(settings|theme|styles|font|rels|docProps|Content_Types)' |\
   apertium-adapt-docx -n |\
   apertium-deswxml "${FORMAT_OPTIONS[@]}" |\
-  if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+  if [[ -z "$TRANSLATION_MEMORY_FILE" ]];
   then cat;
   else lt-tmxproc "$TMCOMPFILE";
   fi | \
-    if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-    sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | \
+    bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
     apertium-rewxml|\
   awk '{punto = index($0, "/>") + 3; cabeza = substr($0, 1, punto-1); cola = substr($0, punto); n1 = substr(cabeza, index(cabeza, "\"")+1); name = substr(n1, 1, index(n1, "\"")-1); gsub("[?]> ", "?>\n", cola); print cola > name;}'
   VUELVE=$(pwd)
   cd "$INPUT_TMPDIR"
-  zip -q -r - . >"$OTRASALIDA"
+  zip -q -r - . >"$TMP_OUT_FILE"
   cd "$VUELVE"
   rm -Rf "$INPUT_TMPDIR"
 
-  if [ "$BORRAFICHERO" = "true" ]; then
+  if [[ "$BORRAFICHERO" = "true" ]]; then
     rm -Rf "$INFILE";
   fi
 
-  if [ "$REDIR" == "" ]; then cat "$OTRASALIDA"; else cat "$OTRASALIDA" > "$SALIDA"; fi
-  rm -Rf "$OTRASALIDA"
+  if [[ -z "$REDIR" ]]; then cat "$TMP_OUT_FILE"; else cat "$TMP_OUT_FILE" > "$OUT_FILE"; fi
+  rm -Rf "$TMP_OUT_FILE"
 }
 
-translate_pptx ()
-{
+translate_pptx () {
+  check_transfuse
+  if [[ $USE_TRANSFUSE = "yes" ]]; then
+    tf-extract -f pptx | bash <(apertium-wblank-mode -z "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then tf-inject; else tf-inject > "$OUT_FILE"; fi
+    return
+  fi
+
   INPUT_TMPDIR=$(mktemp -d "$TMPDIR/apertium.XXXXXXXX")
 
-  locale_utf8
   test_zip
 
-  if [ "$INFILE" = "" ]; then
+  if [[ -z "$INFILE" ]]; then
     INFILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
     cat > "$INFILE"
     BORRAFICHERO="true"
   fi
-  OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
+  TMP_OUT_FILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
-  if [ "$UWORDS" = "no" ]; then
+  if [[ "$UWORDS" = "no" ]]; then
     OPCIONU="-u";
   else OPCIONU="";
   fi
@@ -255,103 +275,95 @@ translate_pptx ()
   find "$INPUT_TMPDIR" -path '**/slides/slide*.xml' |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   apertium-despptx "${FORMAT_OPTIONS[@]}" |\
-  if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+  if [[ -z "$TRANSLATION_MEMORY_FILE" ]];
   then cat;
   else lt-tmxproc "$TMCOMPFILE";
   fi | \
-    if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-    sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | \
+    bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
     apertium-repptx |\
   awk '{punto = index($0, "/>") + 3; cabeza = substr($0, 1, punto-1); cola = substr($0, punto); n1 = substr(cabeza, index(cabeza, "\"")+1); name = substr(n1, 1, index(n1, "\"")-1); gsub("[?]> ", "?>\n", cola); print cola > name;}'
   VUELVE=$(pwd)
   cd "$INPUT_TMPDIR"
-  zip -q -r - . >"$OTRASALIDA"
+  zip -q -r - . >"$TMP_OUT_FILE"
   cd "$VUELVE"
   rm -Rf "$INPUT_TMPDIR"
 
-  if [ "$BORRAFICHERO" = "true" ]; then
+  if [[ "$BORRAFICHERO" = "true" ]]; then
     rm -Rf "$INFILE";
   fi
 
-  if [ "$REDIR" == "" ]; then cat "$OTRASALIDA"; else cat "$OTRASALIDA" > "$SALIDA"; fi
-  rm -Rf "$OTRASALIDA"
+  if [[ -z "$REDIR" ]]; then cat "$TMP_OUT_FILE"; else cat "$TMP_OUT_FILE" > "$OUT_FILE"; fi
+  rm -Rf "$TMP_OUT_FILE"
 }
 
 
-translate_xlsx ()
-{
+translate_xlsx () {
   INPUT_TMPDIR=$(mktemp -d "$TMPDIR/apertium.XXXXXXXX")
 
-  locale_utf8
   test_zip
 
-  if [ "$INFILE" = "" ]; then
+  if [[ -z "$INFILE" ]]; then
     INFILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
     cat > "$INFILE"
     BORRAFICHERO="true"
   fi
-  OTRASALIDA=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
+  TMP_OUT_FILE=$(mktemp "$TMPDIR/apertium.XXXXXXXX")
 
   unzip -q -o -d "$INPUT_TMPDIR" "$INFILE"
   find "$INPUT_TMPDIR" -name "sharedStrings.xml" |\
   awk '{printf "<file name=\"" $0 "\"/>"; PART = $0; while(getline < PART) printf(" %s", $0); printf("\n");}' |\
   apertium-desxlsx "${FORMAT_OPTIONS[@]}" |\
-  if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+  if [[ -z "$TRANSLATION_MEMORY_FILE" ]];
   then cat;
   else lt-tmxproc "$TMCOMPFILE";
   fi | \
-    if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-    sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | \
+    bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
     apertium-rexlsx |\
   awk '{punto = index($0, "/>") + 3; cabeza = substr($0, 1, punto-1); cola = substr($0, punto); n1 = substr(cabeza, index(cabeza, "\"")+1); name = substr(n1, 1, index(n1, "\"")-1); gsub("[?]> ", "?>\n", cola); print cola > name;}'
   VUELVE=$(pwd)
   cd "$INPUT_TMPDIR"
-  zip -q -r - . >"$OTRASALIDA"
+  zip -q -r - . >"$TMP_OUT_FILE"
   cd "$VUELVE"
   rm -Rf "$INPUT_TMPDIR"
 
-  if [ "$BORRAFICHERO" = "true" ]; then
+  if [[ "$BORRAFICHERO" = "true" ]]; then
     rm -Rf "$INFILE";
   fi
 
-  if [ "$REDIR" == "" ]; then cat "$OTRASALIDA"; else cat "$OTRASALIDA" > "$SALIDA"; fi
-  rm -Rf "$OTRASALIDA"
+  if [[ -z "$REDIR" ]]; then cat "$TMP_OUT_FILE"; else cat "$TMP_OUT_FILE" > "$OUT_FILE"; fi
+  rm -Rf "$TMP_OUT_FILE"
+}
+
+translate_html () {
+  check_transfuse
+  if [[ $USE_TRANSFUSE = "yes" ]]; then
+    tf-extract -f html | bash <(apertium-wblank-mode -z "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then tf-inject; else tf-inject > "$OUT_FILE"; fi
+    return
+  fi
+
+  apertium-deshtml "${FORMAT_OPTIONS[@]}" "$INFILE" | \
+    if [[ -z "$TRANSLATION_MEMORY_FILE" ]]; then
+      cat
+    else lt-tmxproc "$TMCOMPFILE";
+    fi | bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then apertium-rehtml; else apertium-rehtml > "$OUT_FILE"; fi
 }
 
 translate_htmlnoent ()
 {
   apertium-deshtml "${FORMAT_OPTIONS[@]}" "$INFILE" | \
-      if [ "$TRANSLATION_MEMORY_FILE" = "" ]; then
-          cat
-      else lt-tmxproc "$TMCOMPFILE";
-      fi | if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-      sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | if [ "$FORMAT" = "none" ]; then
-      if [ "$REDIR" == "" ]; then cat; else cat > "$SALIDA"; fi
-  else
-    if [ "$REDIR" == "" ]; then apertium-rehtml-noent; else apertium-rehtml-noent > "$SALIDA"; fi
-  fi
+    if [[ -z "$TRANSLATION_MEMORY_FILE" ]]; then
+      cat
+    else lt-tmxproc "$TMCOMPFILE";
+    fi | bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then apertium-rehtml-noent; else apertium-rehtml-noent > "$OUT_FILE"; fi
 }
 
 translate_htmlalt ()
 {
   apertium-deshtml-alt "${FORMAT_OPTIONS[@]}" "$INFILE" | \
-      if [ "$TRANSLATION_MEMORY_FILE" = "" ]; then
-          cat
-      else lt-tmxproc "$TMCOMPFILE";
-      fi | if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-      sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  else "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-  fi | if [ "$FORMAT" = "none" ]; then
-      if [ "$REDIR" == "" ]; then cat; else cat > "$SALIDA"; fi
-  else
-    if [ "$REDIR" == "" ]; then apertium-rehtml-noent; else apertium-rehtml-noent > "$SALIDA"; fi
-  fi
+    if [[ -z "$TRANSLATION_MEMORY_FILE" ]]; then
+      cat
+    else lt-tmxproc "$TMCOMPFILE";
+    fi | bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ -z "$REDIR" ]]; then apertium-rehtml-noent; else apertium-rehtml-noent > "$OUT_FILE"; fi
 }
 
 translate_line ()
@@ -360,25 +372,22 @@ translate_line ()
   # lines, even though the parts get output as individual lu's
   apertium-destxt -n "$INFILE" | \
       sed 's/[[:space:]]*\[$/[][/' |\
-      if [ "$TRANSLATION_MEMORY_FILE" = "" ]; then
+      if [[ -z "$TRANSLATION_MEMORY_FILE" ]]; then
           cat
       else
         lt-tmxproc "$TMCOMPFILE";
       fi | \
-          if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-              sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-          else
-            "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-          fi | \
-              if [ "$REDIR" == "" ]; then
+          bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | \
+              if [[ -z "$REDIR" ]]; then
                   apertium-retxt
               else
-                  apertium-retxt > "$SALIDA"
+                  apertium-retxt > "$OUT_FILE"
               fi
 }
 
 
-
+# Require UTF-8 locale
+locale_utf8
 
 ##########################################################
 # Option and argument parsing, setting globals variables #
@@ -396,6 +405,7 @@ DATADIR=$APERTIUM_DATADIR
 TRANSLATION_MEMORY_DIRECTION=$PAIR
 LIST_MODES_AND_EXIT=false
 FORMAT_OPTIONS=()
+NULL_FLUSH=""
 
 # Skip (but store) non-option arguments that come before options:
 declare -a ARGS_PREOPT ARGS_ALL
@@ -420,6 +430,7 @@ while getopts ":uahlf:d:m:o:n" opt; do
     n) FORMAT_OPTIONS+=(-n) ;;
     a) OPTION_TAGGER="-m" ;;
     l) LIST_MODES_AND_EXIT=true ;;
+    z) NULL_FLUSH="-z" ;;
     h) message ;;
     \?) echo "ERROR: Unknown option $OPTARG" >&2; message >&2 ;;
     :) echo "ERROR: $OPTARG requires an argument" >&2; message >&2 ;;
@@ -434,7 +445,7 @@ set -- "${ARGS_PREOPT[@]}" "$@"
 
 case "$#" in
   3)
-    SALIDA=$3
+    OUT_FILE=$3
     REDIR=">"
     INFILE=$2
     PAIR=$1
@@ -475,7 +486,7 @@ fi
 if [[ ! -e "$DATADIR/modes/$PAIR.mode" ]]; then
   echo -n "Error: Mode $PAIR does not exist"
   c=$(find "$DATADIR/modes" -name '*.mode' | wc -l)
-  if [ "$c" -le 1 ]; then
+  if [[ "$c" -le 1 ]]; then
     echo "."
   else
     echo ". Try one of:"
@@ -488,39 +499,39 @@ fi
 
 case "$FORMAT" in
   none)
-    if [ "$UWORDS" = "no" ]; then
+    if [[ "$UWORDS" = "no" ]]; then
       OPTION="-n";
     else OPTION="-g";
     fi
     ;;
-  txt|rtf|html|xpresstag|mediawiki)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+  txt|rtf|xpresstag|mediawiki)
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     ;;
   odt)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_odt
     exit 0
     ;;
   latex)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_latex
     exit 0
     ;;
   latex-raw)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_latex_raw
     exit 0
     ;;
   line)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_line
@@ -529,35 +540,42 @@ case "$FORMAT" in
 
 
   docx)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_docx
     exit 0
     ;;
   xlsx)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_xlsx
     exit 0
     ;;
   pptx)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_pptx
     exit 0
     ;;
+  html)
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
+    else OPTION="-g";
+    fi;
+    translate_html
+    exit 0
+    ;;
   html-noent)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_htmlnoent
     exit 0
     ;;
   html-alt)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
     translate_htmlalt
@@ -565,10 +583,9 @@ case "$FORMAT" in
     ;;
 
   wxml)
-    if [ "$UWORDS" = "no" ]; then OPTION="-n";
+    if [[ "$UWORDS" = "no" ]]; then OPTION="-n";
     else OPTION="-g";
     fi;
-    locale_utf8
     ;;
 
   txtu)
@@ -587,7 +604,7 @@ case "$FORMAT" in
     FORMAT="rtf";
     OPTION="-n";
     MILOCALE=$(locale -a | grep -E -i -v -m1 'utf|^C|^POSIX$')
-    if [ "$MILOCALE" = "" ]; then
+    if [[ "$MILOCALE" = "" ]]; then
       echo "Error: Install a ISO-8859-1 compatible locale in your system";
       exit 1;
     fi
@@ -620,7 +637,6 @@ case "$FORMAT" in
 
   wxmlu)
     OPTION="-n";
-    locale_utf8
     ;;
 
 
@@ -632,37 +648,32 @@ case "$FORMAT" in
     ;;
 esac
 
-if [ -z "$REF" ]
-then
+if [[ -z "$REF" ]]; then
     REF=$FORMAT
 fi
 
 set -e -o pipefail
 
-if [ "$FORMAT" = "none" ]; then
-    cat "$INFILE"
+if [[ "$FORMAT" = "none" ]]; then
+  cat "$INFILE"
 else
   apertium-des$FORMAT "${FORMAT_OPTIONS[@]}" "$INFILE"
-fi | if [ "$TRANSLATION_MEMORY_FILE" = "" ];
+fi | if [[ -z "$TRANSLATION_MEMORY_FILE" ]];
      then
          cat
      else
        lt-tmxproc "$TMCOMPFILE"
-     fi | if [ ! -x "$DATADIR/modes/$PAIR.mode" ]; then
-              sh "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-          else
-            "$DATADIR/modes/$PAIR.mode" "$OPTION" "$OPTION_TAGGER"
-          fi | if [ "$FORMAT" = "none" ]; then
+     fi | bash <(apertium-wblank-mode "$NULL_FLUSH" "$DATADIR/modes/$PAIR.mode") "$OPTION" "$OPTION_TAGGER" | if [[ "$FORMAT" = "none" ]]; then
                    if [ "$REDIR" = "" ]; then
                        cat
                    else
-                     cat > "$SALIDA"
+                     cat > "$OUT_FILE"
                    fi
                else
                  if [ "$REDIR" = "" ]; then
                      apertium-re$FORMAT
                  else
-                   apertium-re$FORMAT > "$SALIDA"
+                   apertium-re$FORMAT > "$OUT_FILE"
                  fi
                fi
 
